138377bd0ab2a50755c87b006872e6b266db6019
corteno <corten.mgs@gmail.com>
2017-06-26 21:11:20 +0200

    - Some stuff

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default() => {
+    return [
+        {title: 'Javascript: The Good parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eoloquent Ruby'}
+    ]
+}
+
